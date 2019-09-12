17bd0df26cfe541a7295a31c9e5b8791aa3cf3e8
fkeck <francois.keck@gmail.com>
2019-09-12 14:25:20 +0200

    Change the system to get the db version list. Now the function gets the updated list from a distant server.

--- R/get_diatbarcode.R
+++ R/get_diatbarcode.R
@@ -1,17 +1,6 @@
 
 dic_version <- function() {
-  dic <- matrix(
-    c("1",      "28-11-2012", "https://data.inra.fr/api/access/datafile/86731?gbrecs=true",
-      "2",      "16-09-2014", "https://data.inra.fr/api/access/datafile/86732?gbrecs=true",
-      "3",      "13-02-2015", "https://data.inra.fr/api/access/datafile/86733?gbrecs=true",
-      "4",      "16-09-2015", "https://data.inra.fr/api/access/datafile/86734?gbrecs=true",
-      "5",      "18-02-2016", "https://data.inra.fr/api/access/datafile/86735?gbrecs=true",
-      "6",      "20-03-2017", "https://data.inra.fr/api/access/datafile/86736?gbrecs=true",
-      "7",      "23-02-2018", "https://data.inra.fr/api/access/datafile/77781?gbrecs=true",
-      "7.1",    "12-02-2019", "https://data.inra.fr/api/access/datafile/83042?gbrecs=true"
-    ), ncol = 3, byrow = TRUE)
-  colnames(dic) <- c("Version", "Date", "URL")
-  dic <- as.data.frame(dic, stringsAsFactors = FALSE)
+  dic <- read.csv("http://www.francoiskeck.fr/work/dic_version.csv", header = TRUE, stringsAsFactors = FALSE)
   return(dic)
 }
 
